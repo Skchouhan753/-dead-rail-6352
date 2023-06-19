bb8336743a516df875fd7ecec8d69581c8f8e9e2
Subhash Kumar Chouhan <104707355+Skchouhan753@users.noreply.github.com>
2023-06-19 13:23:57 +0530

    Add files via upload

--- dribbble/script/login.js
+++ dribbble/script/login.js
@@ -17,7 +17,7 @@ form.addEventListener('submit', (e) => {
         loaingImg.src="https://i.gifer.com/3WGL.gif";
         setTimeout(function(){
             window.location.href="/loginSignup/signup.html";
-        },2000);
+        },2500);
         loading.append(loaingImg,waitText);   
     }
     else{
@@ -41,7 +41,7 @@ form.addEventListener('submit', (e) => {
                 },5000);
                 loading.append(loaingImg,waitText);     
                 }
-            else if(element.email!==form.user_name.value&&element.password!==form.password.value){
+            else if(element.email!==form.user_name.value||element.password!==form.password.value){
                     console.log(element)
                     loaingImg.class="loadingImg"
                     loaingImg.src="https://media.tenor.com/guhB4PpjrmUAAAAM/loading-loading-gif.gif";
